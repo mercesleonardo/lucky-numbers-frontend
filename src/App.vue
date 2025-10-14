77cd69ececa193733b0c1f2b8035a318a43005f8
Leonardo Carvalho <leonardokarvalho@gmail.com>
2025-10-14 15:36:32 -0300

    chore: update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,16 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import AppHeader from '@/components/AppHeader.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue';
 </script>
 
 <template>
-  <AppHeader />
-  <RouterView />
+  <div class="min-h-screen flex flex-col bg-gray-900">
+
+    <AppHeader />
+
+    <RouterView />
+
+    <AppFooter />
+  </div>
 </template>
-
-<style scoped>
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
