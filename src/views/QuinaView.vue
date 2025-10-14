6b5702b5f943dfb072b334c4a15d86483bd7241b
Leonardo Carvalho <leonardokarvalho@gmail.com>
2025-10-14 12:14:38 -0300

    feat: add QuinaView page

---
+++ src/views/QuinaView.vue
@@ -0,0 +1,12 @@
+<template>
+    <GameLayout>
+        <h2 class="text-2xl font-bold text-blue-700 mb-4">Quina</h2>
+        <ResultCard title="Quina" :numbers="[3, 18, 27, 44, 72]" contest="6123" date="13/10/2025" />
+        <!-- Adicione mais informações ou funcionalidades específicas da Quina aqui -->
+    </GameLayout>
+</template>
+
+<script setup lang="ts">
+import GameLayout from '@/components/GameLayout.vue'
+import ResultCard from '@/components/ResultCard.vue'
+</script>
