851a2e287f05f35437edb600f6759ff57ace7590
Leonardo Carvalho <leonardokarvalho@gmail.com>
2025-10-14 12:14:37 -0300

    feat: add GameLayout component for game pages

---
+++ src/components/GameLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="max-w-2xl mx-auto bg-white rounded-lg shadow-lg p-6 mt-6">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+// Componente de layout para páginas de jogos
+</script>
